25fe658070a915b77e21ca043323f4ac6740233d
Samuel Gurr <samuel.gurr@nmfs.local>
2021-10-12 16:26:34 -0400

    feeding rate edit

--- RAnalysis/Scripts/feeding_rates.R
+++ RAnalysis/Scripts/feeding_rates.R
@@ -20,7 +20,8 @@ library(car)
 library(reshape2)
 library(lubridate)
 library(SciViews)
-
+library(reshape2)
+library(SciViews)
 
 # SET WORKING DIRECTORY :::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -133,37 +134,34 @@ for (i in 1:nrow(loop_914_BLANKS)) {
   dat <- clear.rate.914.Blanks %>% 
           dplyr::filter(unique_identifier == loop_914_BLANKS[i,])
     for (j in 2:nrow(dat)) {
-         # ( (4/1000)* (lag(dat$Time._min, n = 1, default = first(Time._min))) ) ln(Cells_ml - (lag(Cells_ml, n = 1, default = first(Cells_ml))))
       dat2 <- dat %>% 
-        dplyr::mutate(diff = Time._min - lag(Time._min, default = first(Time._min))) %>% 
-        dplyr::mutate(ClearRate          = (Cells_ml - lag(Cells_ml, default = first(Cells_ml)))  ) %>% 
-        dplyr::mutate(ClearRate_minus_T0 = (Cells_ml - dat$Cells_ml[1] )) %>% 
+        dplyr::mutate(diff = as.numeric(dat$Time._min) - as.numeric(dat$Time._min[1]) ) %>% 
+        dplyr::mutate(AlgaeLossRatio = as.numeric(dat$Cells_ml[1]) / as.numeric(dat$Cells_ml) ) %>% 
+        dplyr::filter(!AlgaeLossRatio < 1) %>% 
+        dplyr::mutate(ln_AlgaeLossRatio = ln(AlgaeLossRatio)) %>% 
         dplyr::mutate(Time_period = paste((as.numeric(substr(Time._min,1,1)) -1), "0-", Time._min, sep =''))
-      dat2OM <- dat2[-1,]
+      dat2OM <- dat2 %>% dplyr::filter(!Time._min == 0)
     }
-  
   if (nrow(dat2OM) > 0) {
     df       <- data.frame(dat2OM) # name dataframe for this single row
     meanBLANKS.914 <- rbind(meanBLANKS.914,df) #bind to a cumulative list dataframe
     print(meanBLANKS.914) # print to monitor progress
   } else {}
-  
 }
 
-min(meanBLANKS.914$ClearRate_minus_T0)
-max(meanBLANKS.914$ClearRate_minus_T0)
-data.frame( meanBLANKS.914[!is.na(meanBLANKS.914$ClearRate_minus_T0),] %>% 
-             dplyr::filter(ClearRate_minus_T0 < 0) %>% 
+min(meanBLANKS.914$ln_AlgaeLossRatio)
+max(meanBLANKS.914$ln_AlgaeLossRatio)
+data.frame( meanBLANKS.914[!is.na(meanBLANKS.914$ln_AlgaeLossRatio),] %>% 
              dplyr::group_by(pH, substr(Time._min,1,1)) %>% 
              dplyr::summarise(
-               meanCR = mean(ClearRate_minus_T0),
-               sdCR   = sd(ClearRate_minus_T0),
-               seCR   = sd(ClearRate_minus_T0) / sqrt(length(ClearRate_minus_T0)), 
+               meanBlank_ln_AlgaeLoss = mean(ln_AlgaeLossRatio),
+               sdBlank_ln_AlgaeLoss   = sd(ln_AlgaeLossRatio),
+               seBlank_ln_AlgaeLoss   = sd(ln_AlgaeLossRatio) / sqrt(length(ln_AlgaeLossRatio)), 
                n= n()))
 
 
 
-
+  # FR =( V/t * (ln(C_0/C_t ) - A) )/ L
 
 
 
@@ -193,28 +191,32 @@ for (i in 1:nrow(loop_930_BLANKS)) {
   for (j in 2:nrow(dat)) {
     # ( (4/1000)* (lag(dat$Time._min, n = 1, default = first(Time._min))) ) ln(Cells_ml - (lag(Cells_ml, n = 1, default = first(Cells_ml))))
     dat2 <- dat %>% 
-      dplyr::mutate(diff = as.numeric(Time._min) - lag(as.numeric(Time._min), default = first(as.numeric(Time._min)))) %>% 
-      dplyr::mutate(ClearRate = (Cells_ml - lag(Cells_ml, default = first(Cells_ml)))  ) %>% 
-      dplyr::mutate(ClearRate_minus_T0 = (Cells_ml - dat$Cells_ml[1] )) %>% 
+      dplyr::mutate(diff = as.numeric(dat$Time._min) - as.numeric(dat$Time._min[1]) ) %>% 
+      dplyr::arrange(Time._min) %>% 
+      dplyr::mutate(AlgaeLossRatio = as.numeric(dat$Cells_ml[1]) / as.numeric(dat$Cells_ml) ) %>% 
+      dplyr::filter(!AlgaeLossRatio < 1) %>% 
+      dplyr::mutate(ln_AlgaeLossRatio = ln(AlgaeLossRatio)) %>% 
       dplyr::mutate(Time_period = paste((as.numeric(substr(Time._min,1,1)) -1), "0-", Time._min, sep =''))
-    dat2OM <- dat2[-1,]
+    dat2OM <- dat2 %>% dplyr::filter(!Time._min == 0)
   }
   if (nrow(dat2OM) > 0) {
-    df             <- data.frame(dat2OM) # name dataframe for this single row
+    df       <- data.frame(dat2OM) # name dataframe for this single row
     meanBLANKS.930 <- rbind(meanBLANKS.930,df) #bind to a cumulative list dataframe
     print(meanBLANKS.930) # print to monitor progress
   } else {}
 }
 
-data.frame( meanBLANKS.930[!is.na(meanBLANKS.930$ClearRate_minus_T0),] %>% 
-              dplyr::filter(ClearRate_minus_T0 < 0) %>% 
-              dplyr::group_by(pH) %>% 
-              dplyr::summarise(
-                meanCR = mean(ClearRate_minus_T0),
-                sdCR   = sd(ClearRate_minus_T0),
-                seCR   = sd(ClearRate_minus_T0) / sqrt(length(ClearRate_minus_T0)), 
-                n= n()))
-
+min(meanBLANKS.930$ln_AlgaeLossRatio)
+max(meanBLANKS.930$ln_AlgaeLossRatio)
+A_930_BlankMeans <- data.frame( meanBLANKS.930[!is.na(meanBLANKS.930$ln_AlgaeLossRatio),] %>% 
+                      dplyr::group_by(pH, 
+                                      Time._min) %>% 
+                      dplyr::summarise(
+                        meanBlank_ln_AlgaeLoss = mean(ln_AlgaeLossRatio),
+                        sdBlank_ln_AlgaeLoss   = sd(ln_AlgaeLossRatio),
+                        seBlank_ln_AlgaeLoss   = sd(ln_AlgaeLossRatio) / sqrt(length(ln_AlgaeLossRatio)), 
+                        n= n()))
+A_930_BlankMeans
 
 
 
@@ -295,33 +297,34 @@ ggplot(SlopeTable_914, aes(pH , abs(slope) , fill = pH)) +
 
 for (i in 1:nrow(loop_914)) {
   dat <- ClearRate_Master.914 %>% 
-    dplyr::filter(uniq_Identifier == loop_914[i,]) %>% 
+    dplyr::filter(uniq_Identifier == loop_914[1,]) %>% 
     dplyr::arrange(Time._min)
   dat2 <- dat %>% 
-    dplyr::mutate(diff = as.numeric(Time._min) - lag(as.numeric(Time._min), default = first(as.numeric(Time._min)))) %>% 
-    
-    dplyr::mutate(AlgaeLossRatio = lag(as.numeric(Cells_ml), default = first(as.numeric(Cells_ml))) / as.numeric(Cells_ml) ) %>% 
+    #dplyr::mutate(diff = as.numeric(Time._min) - lag(as.numeric(Time._min), default = first(as.numeric(Time._min)))) %>% 
+    dplyr::mutate(diff = as.numeric(dat$Time._min) - as.numeric(dat$Time._min[1]) ) %>% 
+    dplyr::mutate(AlgaeLossRatio = as.numeric(dat$Cells_ml[1]) / as.numeric(dat$Cells_ml) ) %>% 
     dplyr::filter(!AlgaeLossRatio < 1) %>% 
     dplyr::mutate(ln_AlgaeLossRatio = ln(AlgaeLossRatio)) %>% 
    # dplyr::mutate(ClearanceRate =  ( (25/1000) / (diff/60)  * # V / t == Volume of the vessel (in Liters as 25 ml / 1000 ml L-1) and t = time in hours as the diff between the interval sin minutes / 60 mins hour-1
    #                                   ( ln_AlgaeLossRatio ) / Length.um. ) ) %>% 
-    dplyr::mutate(ClearanceRate = ( (25/1000) * # V / t == Volume of the vessel (in Liters as 25 ml / 1000 ml L-1) and t = time in hours as the diff between the interval sin minutes / 60 mins hour-1
-                                       ( ln_AlgaeLossRatio *  (diff/60) - 0) ) / (Length.um./1000) ) %>% 
+    dplyr::mutate(ClearanceRate_L_hour_meter = ( (25/1000) * # V / t == Volume of the vessel (in Liters as 25 ml / 1000 ml L-1) and t = time in hours as the diff between the interval sin minutes / 60 mins hour-1
+                                       (( ln_AlgaeLossRatio /  (diff/60) - 0))) / (Length.um./1000000) ) %>% 
     dplyr::mutate(Time_period = paste((as.numeric(substr(Time._min,1,1)) -1), "0-", Time._min, sep =''))
-  
-  if (nrow(dat2) > 0) {
-    ClearRate.table           <- data.frame(matrix(nrow = nrow(dat2), ncol = 9)) # create dataframe to save cumunalitively during for loop
-    colnames(ClearRate.table) <- c('Date', 'ID', 'pH', 'Replicate', 'Num','Run','Time_period', 'AlgaeLossRatio', 'ClearanceRate') # names for comuns in the for loop
+    dat2OM <- dat2 %>% dplyr::filter(!Time._min == 0)
     
-    ClearRate.table$Date          <- dat2$Date
-    ClearRate.table$ID            <- loop_914[i,]
-    ClearRate.table$pH            <- gsub("_.*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Replicate     <- gsub("^(?:[^_]+_){4}([^_]+).*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Num           <- gsub("^(?:[^_]+_){6}([^_]+).*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Run           <- gsub("^(?:[^_]+_){2}([^_]+).*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Time_period   <- paste((as.numeric(substr(dat2$Time._min,1,1)) -1), "0-", dat2$Time._min, sep ='')
-    ClearRate.table$AlgaeLossRatio <- dat2$AlgaeLossRatio
-    ClearRate.table$ClearanceRate <- dat2$ClearanceRate
+  if (nrow(dat2OM) > 0) {
+    ClearRate.table           <- data.frame(matrix(nrow = nrow(dat2OM), ncol = 9)) # create dataframe to save cumunalitively during for loop
+    colnames(ClearRate.table) <- c('Date', 'ID', 'pH', 'Replicate', 'Num','Run','Time_period', 'AlgaeLossRatio', 'ClearanceRate_L_hour_meter') # names for comuns in the for loop
+    
+    ClearRate.table$Date                       <- dat2OM$Date
+    ClearRate.table$ID                         <- loop_914[i,]
+    ClearRate.table$pH                         <- gsub("_.*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Replicate                  <- gsub("^(?:[^_]+_){4}([^_]+).*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Num                        <- gsub("^(?:[^_]+_){6}([^_]+).*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Run                        <- gsub("^(?:[^_]+_){2}([^_]+).*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Time_period                <- paste((as.numeric(substr(dat2OM$Time._min,1,1)) -1), "0-", dat2OM$Time._min, sep ='')
+    ClearRate.table$AlgaeLossRatio             <- dat2OM$AlgaeLossRatio
+    ClearRate.table$ClearanceRate_L_hour_meter <- dat2OM$ClearanceRate_L_hour_meter
     
     df       <- data.frame(ClearRate.table) # name dataframe for this single row
     df_total.914 <- rbind(df_total.914,df) #bind to a cumulative list dataframe
@@ -331,18 +334,18 @@ for (i in 1:nrow(loop_914)) {
 }
 
 ClearRates_914_Means <- df_total.914 %>% 
-  dplyr::filter(!ClearanceRate %in% '-Inf') %>% 
+  dplyr::filter(!ClearanceRate_L_hour_meter %in% '-Inf') %>% 
   dplyr::group_by(pH, substr(Time_period,4,4)) %>% 
   dplyr::summarise(
-    meanCR = mean(abs(ClearanceRate)),
-    sdCR   = sd(abs(ClearanceRate)), 
-    seCR   = sd(abs(ClearanceRate)) / sqrt(length(abs(ClearanceRate))), 
+    meanCR = mean(abs(ClearanceRate_L_hour_meter)),
+    sdCR   = sd(abs(ClearanceRate_L_hour_meter)), 
+    seCR   = sd(abs(ClearanceRate_L_hour_meter)) / sqrt(length(abs(ClearanceRate_L_hour_meter))), 
     n = n()) %>% 
   na.omit()
 colnames(ClearRates_914_Means)[2] <- 'Time_period'
 
 summary(lmer(meanCR~pH+ (1|Time_period), data=ClearRates_914_Means))
-summary(aov(lm(meanCR~pH, data=ClearRates_914_Means)))
+summary(aov(lm(meanCR~pH*Time_period, data=ClearRates_914_Means)))
 
 ClearRates_914_Means %>% 
   #dplyr::filter(!Time_period %in% c('40-50', '50-60')) %>% 
@@ -377,10 +380,9 @@ ClearRates_914_Means %>%
 
 
 # Clearance rate anlaysis for 9/30 data
-ClearRate_Master.930 <- ClearRate_Master %>% 
+ClearRate_Master.930       <- ClearRate_Master %>% 
   dplyr::filter(Date %in% 20210930) %>% 
   dplyr::mutate(uniq_Identifier = paste(pH, Fed_Unfed, "Run", Run, "Rep",Replicate, "Num", Number, sep='_'))
-
 df_total.930              <- data.frame() # start dataframe 
 loop_930 <- as.data.frame(unique(ClearRate_Master.930$uniq_Identifier)) %>% dplyr::rename(ID = "unique(ClearRate_Master.930$uniq_Identifier)")
 
@@ -441,31 +443,40 @@ ggplot(aes(pH , abs(slope) , fill = pH)) +
 
 
 for (i in 1:nrow(loop_930)) {
-    dat <- ClearRate_Master.930 %>% 
+    dat        <- ClearRate_Master.930 %>% 
       dplyr::filter(uniq_Identifier == loop_930[i,]) %>% 
       dplyr::arrange(Time._min)
-    dat2 <- dat %>% 
-      dplyr::mutate(diff = as.numeric(Time._min) - lag(as.numeric(Time._min), default = first(as.numeric(Time._min)))) %>% 
-      dplyr::mutate(AlgaeLossRatio = as.numeric(Cells_ml) / lag(as.numeric(Cells_ml), default = first(as.numeric(Cells_ml)))) %>% 
-      dplyr::filter(AlgaeLossRatio < 1) %>% 
-      dplyr::mutate(ClearanceRate =  ( (25/1000) / (diff/60)  * # V / t == Volume of the vessel (in Liters as 25 ml / 1000 ml L-1) and t = time in hours as the diff between the interval sin minutes / 60 mins hour-1
-                                     ( ln(AlgaeLossRatio) ) / Length.um. ) ) %>% 
-      dplyr::mutate(Time_period = paste((as.numeric(substr(Time._min,1,1)) -1), "0-", Time._min, sep =''))
     
-    if (nrow(dat2) > 0) {
-    ClearRate.table           <- data.frame(matrix(nrow = nrow(dat2), ncol = 10)) # create dataframe to save cumunalitively during for loop
-    colnames(ClearRate.table) <- c('Date', 'ID', 'pH', 'Fed_Unfed', 'Replicate', 'Num','Run','Time_period', 'AlgaeLossRatio', 'ClearanceRate') # names for comuns in the for loop
+    C0 <- as.numeric(dat$Cells_ml)[1]
     
-    ClearRate.table$Date          <- dat2$Date
-    ClearRate.table$ID            <- loop_930[i,]
-    ClearRate.table$pH            <- gsub("_.*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Fed_Unfed     <- gsub("^(?:[^_]+_){1}([^_]+).*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Replicate     <- gsub("^(?:[^_]+_){5}([^_]+).*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Num           <- gsub("^(?:[^_]+_){7}([^_]+).*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Run           <- gsub("^(?:[^_]+_){3}([^_]+).*", "\\1", ClearRate.table$ID)
-    ClearRate.table$Time_period   <- paste((as.numeric(substr(dat2$Time._min,1,1)) -1), "0-", dat2$Time._min, sep ='')
-    ClearRate.table$AlgaeLossRatio <- dat2$AlgaeLossRatio
-    ClearRate.table$ClearanceRate <- dat2$ClearanceRate
+    dat_blanks <- merge(dat, A_930_BlankMeans)
+    dat2       <- dat_blanks %>% 
+        #dplyr::mutate(diff = as.numeric(Time._min) - lag(as.numeric(Time._min), default = first(as.numeric(Time._min)))) %>% 
+        # dplyr::mutate(diff = as.numeric(dat_blanks$Time._min) - as.numeric(dat$Time._min[1]) ) %>% 
+        dplyr::mutate(AlgaeLossRatio = C0 / as.numeric(dat_blanks$Cells_ml) ) %>% 
+        dplyr::filter(!AlgaeLossRatio < 1) %>% 
+        dplyr::mutate(ln_AlgaeLossRatio = ln(AlgaeLossRatio)) %>% 
+        # dplyr::mutate(ClearanceRate =  ( (25/1000) / (diff/60)  * # V / t == Volume of the vessel (in Liters as 25 ml / 1000 ml L-1) and t = time in hours as the diff between the interval sin minutes / 60 mins hour-1
+        #                                   ( ln_AlgaeLossRatio ) / Length.um. ) ) %>% 
+        dplyr::mutate(ClearanceRate_L_hour_mm = ( (25/1000) * # V / t == Volume of the vessel (in Liters as 25 ml / 1000 ml L-1) and t = time in hours as the diff between the interval sin minutes / 60 mins hour-1
+                                                        ( ( ln_AlgaeLossRatio /  ((as.numeric(Time._min))/60)) - meanBlank_ln_AlgaeLoss)  ) / (Length.um./1000) ) %>% 
+        dplyr::mutate(Time_period = paste((as.numeric(substr(Time._min,1,1)) -1), "0-", Time._min, sep =''))
+    dat2OM <- dat2 %>% dplyr::filter(!Time._min == 0)
+    
+    if (nrow(dat2OM) > 0) {
+    ClearRate.table           <- data.frame(matrix(nrow = nrow(dat2OM), ncol = 10)) # create dataframe to save cumunalitively during for loop
+    colnames(ClearRate.table) <- c('Date', 'ID', 'pH', 'Fed_Unfed', 'Replicate', 'Num','Run','Time_period', 'AlgaeLossRatio', 'ClearanceRate_L_hour_mm') # names for comuns in the for loop
+    
+    ClearRate.table$Date                       <- dat2OM$Date
+    ClearRate.table$ID                         <- loop_930[i,]
+    ClearRate.table$pH                         <- gsub("_.*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Fed_Unfed                  <- gsub("^(?:[^_]+_){1}([^_]+).*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Replicate                  <- gsub("^(?:[^_]+_){5}([^_]+).*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Num                        <- gsub("^(?:[^_]+_){7}([^_]+).*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Run                        <- gsub("^(?:[^_]+_){3}([^_]+).*", "\\1", ClearRate.table$ID)
+    ClearRate.table$Time_period                <- paste((as.numeric(substr(dat2OM$Time._min,1,1)) -1), "0-", dat2OM$Time._min, sep ='')
+    ClearRate.table$AlgaeLossRatio             <- dat2OM$AlgaeLossRatio
+    ClearRate.table$ClearanceRate_L_hour_mm <- dat2OM$ClearanceRate_L_hour_mm
     
     df       <- data.frame(ClearRate.table) # name dataframe for this single row
     df_total.930 <- rbind(df_total.930,df) #bind to a cumulative list dataframe
@@ -474,14 +485,13 @@ for (i in 1:nrow(loop_930)) {
     else {}
 }
 
-
 ClearRates_930_Means <- df_total.930 %>% 
-  dplyr::filter(!ClearanceRate %in% '-Inf') %>% 
+  dplyr::filter(!ClearanceRate_L_hour_mm %in% 'NaN') %>% 
   dplyr::group_by(pH, Fed_Unfed, Time_period) %>% 
   dplyr::summarise(
-    meanCR = mean(abs(ClearanceRate)),
-    sdCR   = sd(abs(ClearanceRate)), 
-    seCR   = sd(abs(ClearanceRate)) / sqrt(length(abs(ClearanceRate))), 
+    meanCR = mean(ClearanceRate_L_hour_mm),
+    sdCR   = sd(ClearanceRate_L_hour_mm), 
+    seCR   = sd(ClearanceRate_L_hour_mm) / sqrt(length(ClearanceRate_L_hour_mm)), 
     n = n()) %>% 
   na.omit()
 ClearRates_930_Means
@@ -491,7 +501,7 @@ summary(aov(lm(meanCR~pH*Fed_Unfed, data=ClearRates_930_Means)))
 
 ClearRates_930_Means$pH_feed <- paste(ClearRates_930_Means$pH, ClearRates_930_Means$Fed_Unfed, sep='_')
 ClearRates_930_Means %>% 
-dplyr::filter(!Time_period %in% c('40-50', '50-60')) %>% 
+#dplyr::filter(!Time_period %in% c('40-50', '50-60')) %>% 
 ggplot(aes(x=pH_feed , y=meanCR, fill = pH_feed)) +
   geom_bar(position=position_dodge(), aes(y=meanCR), stat="identity", alpha=0.5) +
   scale_fill_manual("Treatment", values = c("8_fed" = "grey70", "7.5_fed" = "grey 10", "8_unfed" = "grey 70", "7.5_unfed" = "black")) +
@@ -505,9 +515,9 @@ ggplot(aes(x=pH_feed , y=meanCR, fill = pH_feed)) +
 
 df_total.930 %>% 
   dplyr::mutate(pH_feed = paste(pH, Fed_Unfed, sep ='_')) %>% 
-  dplyr::filter(!ClearanceRate %in% '-Inf') %>% 
+  #dplyr::filter(!ClearanceRate_L_hour_mm %in% '-Inf') %>% 
   #dplyr::filter(!Time_period %in% c('40-50', '50-60')) %>% 
-  ggplot(aes(pH_feed , abs(ClearanceRate) , fill = pH_feed)) +
+  ggplot(aes(pH_feed , ClearanceRate_L_hour_mm , fill = pH_feed)) +
   theme(panel.grid=element_blank()) +
   geom_boxplot(size=0.2, alpha=0.1, aes(fill=pH_feed)) +
   scale_fill_manual(values=c("white", "grey70","grey40", "grey20")) +
